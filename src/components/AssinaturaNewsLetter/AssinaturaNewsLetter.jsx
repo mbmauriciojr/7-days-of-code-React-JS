839ead8e93ae6ef7899b3442acbd4de2f77551b8
Marcelo Maurício <mmauricio.design@gmail.com>
2022-04-04 10:58:32 -0300

    Created the component AssinaturaNewsLetter

---
+++ src/components/AssinaturaNewsLetter/AssinaturaNewsLetter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AssinaturaNewsLetter() {
+  return (
+    <div>
+      <input type="email" placeholder="Insira o seu email" />
+      <button type="button">Assinar newsletter</button>
+    </div>
+  );
+}
+
+export default AssinaturaNewsLetter;
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Menu from '../../components/Menu/Menu';
+import AssinaturaNewsLetter from '../../components/AssinaturaNewsLetter/AssinaturaNewsLetter';
 
 function HomePage() {
   return (
     <main>
       <Menu />
+      <AssinaturaNewsLetter />
     </main>
   );
 }
