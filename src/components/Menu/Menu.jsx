738f2bd415d0d7d8a03a508f92b52445557a1d5b
Marcelo Maurício <mmauricio.design@gmail.com>
2022-04-04 10:43:53 -0300

    Inserted some options to the navbar in the Menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -5,9 +5,16 @@ function Menu() {
   return (
     <header>
       <nav>
-        <Link to="/">Home</Link>
+        <Link to="/">Como fazer</Link>
+        <p>/</p>
+        <Link to="/ofertas">Ofertas</Link>
+        <p>/</p>
+        <Link to="/depoimentos">Depoimentos</Link>
+        <p>/</p>
+        <Link to="/videos">Vídeos</Link>
+        <p>/</p>
+        <Link to="/meu-carinho">Meu Carrinho</Link>
       </nav>
-      <p>Under Construction</p>
     </header>
   );
 }
