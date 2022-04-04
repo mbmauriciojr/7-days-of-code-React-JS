506e67bf51b8199d871bf8d71d90b73f822bcf88
Marcelo Maurício <mmauricio.design@gmail.com>
2022-04-04 10:09:27 -0300

    Created the Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+
 import './App.css';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Under construction!</p>
-      </header>
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <main>
+      <p>Under Construction</p>
+    </main>
+  );
+}
+
+export default HomePage;
