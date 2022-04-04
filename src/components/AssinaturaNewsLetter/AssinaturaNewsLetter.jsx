b7c61ecdae1d25307551e5cb4a53cd53612e49d9
Marcelo Maurício <mmauricio.design@gmail.com>
2022-04-04 15:40:18 -0300

    Created more information to AssinaturaNewsLetter component

--- src/components/AssinaturaNewsLetter/AssinaturaNewsLetter.jsx
+++ src/components/AssinaturaNewsLetter/AssinaturaNewsLetter.jsx
@@ -4,12 +4,35 @@ import Button from './styles';
 
 function AssinaturaNewsLetter() {
   return (
-    <form action="/">
-      <input type="email" placeholder="Insira o seu email" />
-      <Button type="button">
-        Assinar newsletter
-      </Button>
-    </form>
+    <div>
+      <div>
+        <section>
+          <h1>
+            Sua casa com as
+            <br />
+            <strong>
+              melhores
+              <br />
+              plantas
+            </strong>
+          </h1>
+        </section>
+        <section>
+          <p>
+            Encontre aqui uma vasta seleção de plantas para decorar
+            a sua casa e torná-lo uma pessoa mais feliz no seu dia
+            a dia. Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.
+          </p>
+        </section>
+      </div>
+
+      <form action="/">
+        <input type="email" placeholder="Insira o seu email" />
+        <Button type="button">
+          Assinar newsletter
+        </Button>
+      </form>
+    </div>
   );
 }
 
