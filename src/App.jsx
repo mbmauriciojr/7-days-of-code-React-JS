2642bec5408eedc89c628446a9613ea19ac6bfdf
Marcelo Maurício <mmauricio.design@gmail.com>
2022-04-04 14:03:47 -0300

    Fix: change the way to use the name of the HomPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import './App.css';
-import HomePage from './pages/HomePage';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
